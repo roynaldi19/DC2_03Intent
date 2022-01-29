7dfde6e12f007eab1cd0f1e27d7ab7856e1f259e
roynaldi19 <roynaldi19@gmail.com>
2022-01-29 13:27:47 +0700

    Initial commit

---
+++ app/src/main/java/com/roynaldi19/bfaa01_02intent/MoveActivity.kt
@@ -0,0 +1,11 @@
+package com.roynaldi19.bfaa01_02intent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MoveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move)
+    }
+}
---
+++ app/src/main/java/com/roynaldi19/bfaa01_02intent/Person.kt
@@ -0,0 +1,12 @@
+package com.roynaldi19.bfaa01_02intent
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Person(
+    val name: String?,
+    val age: Int?,
+    val email: String?,
+    val city: String?
+) : Parcelable
